7ffbb8ddbc226d63e75591e8c951b1467b02d99c
joel-danjuma <leojjad@gmail.com>
2025-11-12 20:18:01 +0000

    fix: resolve multiple Alembic migration heads by fixing revision chain
    - Change down_revision from 'a1b2c3d4e5f6' to '3c4d5e6f7g8h' in b2c3d4e5f6g7 migration
    - Creates linear migration chain: a1b2c3d4e5f6 -> 3c4d5e6f7g8h -> b2c3d4e5f6g7
    - Fixes 'Multiple head revisions' error when running 'alembic upgrade head'
    - Ensures migrations can be applied sequentially without conflicts

--- alembic/versions/b2c3d4e5f6g7_add_policy_premium_to_virtual_account_transactions.py
+++ alembic/versions/b2c3d4e5f6g7_add_policy_premium_to_virtual_account_transactions.py
@@ -1,7 +1,7 @@
 """add policy_premium to virtual_account_transactions
 
 Revision ID: b2c3d4e5f6g7
-Revises: a1b2c3d4e5f6
+Revises: 3c4d5e6f7g8h
 Create Date: 2025-11-12 19:50:57.000000
 
 """
@@ -11,7 +11,7 @@ import sqlalchemy as sa
 
 # revision identifiers, used by Alembic.
 revision = 'b2c3d4e5f6g7'
-down_revision = 'a1b2c3d4e5f6'
+down_revision = '3c4d5e6f7g8h'
 branch_labels = None
 depends_on = None
 
